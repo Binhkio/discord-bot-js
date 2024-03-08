4cc71eaa824fab47f3de1e8dec1885fc425c4ab0
Binhkio <quan.va205115@gmail.com>
2024-03-08 12:48:36 +0700

    update server

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send("OK");
 });
 
+app.get('/keep-alive', (req, res) => {
+    res.send("Bot still alive");
+});
+
 function keepAlive() {
     app.listen(PORT, () => {
         console.log(`App is listening on port ${PORT}`);
